649980a8cdb78a3366948cfc6760fcd082ea08f3
Samuel Sanchez <wearycodes@gmail.com>
2023-12-01 18:22:03 -0600

    is it tweakin

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,9 +1,16 @@
 // Write your tests here
 import React from "react"
 import AppClass from "./AppClass"
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 test('The page renders', () => {
   render(<AppClass />)
   const SubmitButton = screen.getByRole('button')
   expect(SubmitButton).toBeVisible()
+})
+test('Clicking submit without email causes error', () => {
+  render(<AppClass />)
+  const SubmitButton = screen.getByRole('button')
+  fireEvent.click(SubmitButton)
+  const errorText = screen.getByText(/email is required/i)
+  expect(errorText).toBeVisible()
 })
