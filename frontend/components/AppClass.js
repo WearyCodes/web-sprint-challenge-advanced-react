1882a55b4dc5b1e6ebdb166f9a67379811e2106d
Samuel Sanchez <wearycodes@gmail.com>
2023-12-01 18:33:06 -0600

    backwards lol

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -42,32 +42,32 @@ export default class AppClass extends React.Component {
         y = 1;
         break
       case 1: 
-        x = 2;
-        y = 1;
+        x = 1;
+        y = 2;
         break
       case 2:
-        x = 3;
-        y = 1;
+        x = 1;
+        y = 3;
         break
       case 3:
-        x = 2;
-        y = 1;
+        x = 1;
+        y = 2;
         break
       case 4:
         x = 2;
         y = 2;
         break
       case 5 :
-        x = 2;
-        y = 3;
-        break
-      case 6:
-        x = 3;
-        y = 1;
-        break
-      case 7:
         x = 3;
         y = 2;
+        break
+      case 6:
+        x = 1;
+        y = 3;
+        break
+      case 7:
+        x = 2;
+        y = 3;
         break
       case 8:
         x = 3;
